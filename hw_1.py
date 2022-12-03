38cc11403f1298ca2a00d7be93cd54357c99fa5b
IvanFed3 <wanua3@gmail.com>
2022-12-03 21:04:31 +0200

    Поправив згідно коментарів

--- hw_1.py
+++ hw_1.py
@@ -3,19 +3,25 @@ class Product:
         self.name = name
         self.price = price
 
+    def __repr__(self) -> str:
+        return self.name
+
     def get_total(self, quantity) -> float:
-        return self.price*quantity
+        return round(self.price * quantity, 2)
 
 
 class ShoppingCart:
-    list = []
+    list_shopping_cart_product = []
+    list_shopping_cart_quantity = []
 
-    def add(self, product, quantity: int = 1) -> None:
-        self.list.append([product.name, quantity, product.price])
+    def add(self, product_name, quantity: int = 1) -> None:
+        self.list_shopping_cart_product.append(product_name)
+        self.list_shopping_cart_quantity.append(quantity)
+        print(self.list_shopping_cart_product)
 
     def get_total(self) -> float:
-        sum_1 = 0
-        for i in self.list:
-            sum_1 += i[1]*i[2]
-        return sum_1
-    
+        sum_shopping_cart = 0
+        for i in range(len(self.list_shopping_cart_product)):
+            sum_shopping_cart += self.list_shopping_cart_product[i].get_total(self.list_shopping_cart_quantity[i])
+        print(sum_shopping_cart)
+        return round(sum_shopping_cart, 2)
