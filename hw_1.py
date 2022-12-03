ced934f198ff2b59d6a2a2cfd622fe8f10a5fb10
IvanFed3 <wanua3@gmail.com>
2022-12-03 21:24:15 +0200

    Видалив зайві прінти

--- hw_1.py
+++ hw_1.py
@@ -17,11 +17,9 @@ class ShoppingCart:
     def add(self, product_name, quantity: int = 1) -> None:
         self.list_shopping_cart_product.append(product_name)
         self.list_shopping_cart_quantity.append(quantity)
-        print(self.list_shopping_cart_product)
 
     def get_total(self) -> float:
         sum_shopping_cart = 0
         for i in range(len(self.list_shopping_cart_product)):
             sum_shopping_cart += self.list_shopping_cart_product[i].get_total(self.list_shopping_cart_quantity[i])
-        print(sum_shopping_cart)
         return round(sum_shopping_cart, 2)
