3f3eb7b6a9c260bc57a882c81b394243d3577f78
IvanFed3 <wanua3@gmail.com>
2022-12-07 22:25:31 +0200

    Поправлений варіант

--- hw2.py
+++ hw2.py
@@ -51,10 +51,10 @@ class ShoppingCart:
             self.quantities.append(quantity)
 
     def __add__(self, other):
+        new_cart = ShoppingCart()
+        new_cart.products = self.products[:]
+        new_cart.quantities = self.quantities[:]
         if isinstance(other, ShoppingCart):
-            new_cart = ShoppingCart()
-            new_cart.products = self.products[:]
-            new_cart.quantities = self.quantities[:]
             for product_1, product_2 in zip(self.products, other.products):
                 id_product_1 = self.products.index(product_1)
                 id_product_2 = other.products.index(product_2)
@@ -63,26 +63,12 @@ class ShoppingCart:
                 else:
                     new_cart.products.append(other.products[id_product_2])
                     new_cart.quantities.append(other.quantities[id_product_2])
-            return new_cart
         else:
-            self.add_product(other)
+            new_cart.add_product(other)
+        return new_cart
 
     def get_total(self) -> float:
         sum_cart = 0
         for product, quantity in zip(self.products, self.quantities):
             sum_cart += product.get_total(quantity)
         return round(sum_cart, 2)
-
-"""Дані для перевірки"""
-apple = Product('apple', 10)
-apple_1 = Product('apple', 11)
-lemon = Product('lemon', 8, 2)
-banana = Product('banana', 112, 4)
-Cart_1 = ShoppingCart()
-Cart_1.add_product(lemon)
-Cart_1.add_product(apple)
-Cart_2 = apple_1 + banana
-Cart_3 = ShoppingCart()
-Cart_3 = Cart_2 + lemon
-
-print(type(Cart_3)) # видає NoneType
