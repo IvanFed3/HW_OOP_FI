154e2634e1a5064e2177dcd3286090bd24e01b05
IvanFed3 <wanua3@gmail.com>
2022-12-10 08:50:06 +0200

    Update

--- hw2.py
+++ hw2.py
@@ -10,6 +10,11 @@ class Product:
     def __float__(self) -> float:
         return self.price
 
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Product):
+            return self.name == other.name and self.price == other.price
+        return False
+
     def __add__(self, other):
         if self.name == other.name and self.price == other.price:
             self.unit += other.unit
@@ -41,11 +46,9 @@ class ShoppingCart:
     def add_product(self, product, quantity: float = None) -> None:
         if quantity is None:
             quantity = product.unit
-        for i in self.products:
-            id_product = self.products.index(i)
-            if product == self.products[id_product]:
-                self.quantities[id_product] += quantity
-                break
+        if product in self.products:
+            id_product = self.products.index(product)
+            self.quantities[id_product] += quantity
         else:
             self.products.append(product)
             self.quantities.append(quantity)
@@ -55,12 +58,13 @@ class ShoppingCart:
         new_cart.products = self.products[:]
         new_cart.quantities = self.quantities[:]
         if isinstance(other, ShoppingCart):
-            for product_1, product_2 in zip(self.products, other.products):
-                id_product_1 = self.products.index(product_1)
-                id_product_2 = other.products.index(product_2)
-                if product_2 == product_1:
+            for product_1 in other.products:
+                if product_1 in new_cart.products:
+                    id_product_1 = new_cart.products.index(product_1)
+                    id_product_2 = other.products.index(product_1)
                     new_cart.quantities[id_product_1] += other.quantities[id_product_2]
                 else:
+                    id_product_2 = other.products.index(product_1)
                     new_cart.products.append(other.products[id_product_2])
                     new_cart.quantities.append(other.quantities[id_product_2])
         else:
@@ -72,3 +76,18 @@ class ShoppingCart:
         for product, quantity in zip(self.products, self.quantities):
             sum_cart += product.get_total(quantity)
         return round(sum_cart, 2)
+
+apple = Product('apple', 10)
+apple_1 = Product('apple', 10, 8)
+lemon = Product('lemon', 8, 2)
+banana = Product('banana', 112, 4)
+cherry = Product('cherry',156)
+Cart_1 = ShoppingCart()
+Cart_1.add_product(lemon)
+Cart_1.add_product(apple)
+Cart_1.add_product(cherry)
+Cart_2 = apple_1 + banana
+Cart_2 += apple
+Cart_3 = Cart_2 + Cart_1
+
+print(Cart_3.products, Cart_3.quantities)
